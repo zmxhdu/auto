92c2e42573075eeac570d5e69a6342995c1caaf9
梦轩 周 <zmxhdu@126.com>
2019-07-01 21:30:22 +0800

    glyphicon

--- autotest/autotest/settings.py
+++ autotest/autotest/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
-#STATIC_ROOT = ''
+#STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATIC_ROOT = ''
 #HERE = os.path.dirname(os.path.abspath(__file__))
 #HERE = os.path.join(HERE, '../')
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
